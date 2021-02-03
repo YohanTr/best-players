fe6912224199d048ea8c207aa8ebe9a1782cc926
Yohan Tahir <yohantahir@gmail.com>
2021-02-03 13:20:05 +0100

    create entity

---
+++ src/Entity/Country.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CountryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CountryRepository::class)
+ */
+class Country
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Player::class, mappedBy="country")
+     */
+    private $players;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Club::class, mappedBy="country")
+     */
+    private $clubs;
+
+    public function __construct()
+    {
+        $this->players = new ArrayCollection();
+        $this->clubs = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Player[]
+     */
+    public function getPlayers(): Collection
+    {
+        return $this->players;
+    }
+
+    public function addPlayer(Player $player): self
+    {
+        if (!$this->players->contains($player)) {
+            $this->players[] = $player;
+            $player->setCountry($this);
+        }
+
+        return $this;
+    }
+
+    public function removePlayer(Player $player): self
+    {
+        if ($this->players->removeElement($player)) {
+            // set the owning side to null (unless already changed)
+            if ($player->getCountry() === $this) {
+                $player->setCountry(null);
+            }
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Club[]
+     */
+    public function getClubs(): Collection
+    {
+        return $this->clubs;
+    }
+
+    public function addClub(Club $club): self
+    {
+        if (!$this->clubs->contains($club)) {
+            $this->clubs[] = $club;
+            $club->setCountry($this);
+        }
+
+        return $this;
+    }
+
+    public function removeClub(Club $club): self
+    {
+        if ($this->clubs->removeElement($club)) {
+            // set the owning side to null (unless already changed)
+            if ($club->getCountry() === $this) {
+                $club->setCountry(null);
+            }
+        }
+
+        return $this;
+    }
+}
---
+++ src/Entity/Player.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PlayerRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=PlayerRepository::class)
+ */
+class Player
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $age;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $gamePlayed;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $goalScored;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $keyPass;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Club::class, inversedBy="players")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $club;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Country::class, inversedBy="players")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $country;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAge(): ?int
+    {
+        return $this->age;
+    }
+
+    public function setAge(int $age): self
+    {
+        $this->age = $age;
+
+        return $this;
+    }
+
+    public function getGamePlayed(): ?int
+    {
+        return $this->gamePlayed;
+    }
+
+    public function setGamePlayed(int $gamePlayed): self
+    {
+        $this->gamePlayed = $gamePlayed;
+
+        return $this;
+    }
+
+    public function getGoalScored(): ?int
+    {
+        return $this->goalScored;
+    }
+
+    public function setGoalScored(int $goalScored): self
+    {
+        $this->goalScored = $goalScored;
+
+        return $this;
+    }
+
+    public function getKeyPass(): ?int
+    {
+        return $this->keyPass;
+    }
+
+    public function setKeyPass(int $keyPass): self
+    {
+        $this->keyPass = $keyPass;
+
+        return $this;
+    }
+
+    public function getClub(): ?Club
+    {
+        return $this->club;
+    }
+
+    public function setClub(?Club $club): self
+    {
+        $this->club = $club;
+
+        return $this;
+    }
+
+    public function getCountry(): ?Country
+    {
+        return $this->country;
+    }
+
+    public function setCountry(?Country $country): self
+    {
+        $this->country = $country;
+
+        return $this;
+    }
+}
