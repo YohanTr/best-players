cedfce0d6c7362f9f3136d2a321a343f02e9500f
Yohan Tahir <yohantahir@gmail.com>
2021-02-03 11:53:38 +0100

    install webpack, faker, maker-bundle

---
+++ assets/bootstrap.js
@@ -0,0 +1,4 @@
+import { startStimulusApp } from '@symfony/stimulus-bridge';
+import '@symfony/autoimport';
+
+// Registers Stimulus controllers from controllers.json and in the controllers/ directory
