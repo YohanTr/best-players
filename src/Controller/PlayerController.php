27382664eca31f752bc74d5fe14a1a6be6969a50
Yohan Tahir <yohantahir@gmail.com>
2021-02-04 00:06:50 +0100

    playerType

--- src/Controller/PlayerController.php
+++ src/Controller/PlayerController.php
@@ -2,14 +2,42 @@
 
 namespace App\Controller;
 
+use App\Entity\Player;
+use App\Form\PlayerType;
 use App\Repository\PlayerRepository;
-use Cocur\Slugify\Slugify;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
 
 class PlayerController extends AbstractController
 {
+
+    /**
+     * @Route("/player/new", name="player_new")
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request) : Response
+    {
+        $player = new Player();
+        $form = $this->createForm(PlayerType::class, $player);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $player = $form->getData();
+
+             $entityManager = $this->getDoctrine()->getManager();
+             $entityManager->persist($player);
+             $entityManager->flush();
+
+            return $this->redirectToRoute('app_home');
+        }
+        return $this->render('player/new.html.twig', [
+            "form" => $form->createView(),
+        ]);
+    }
+
     /**
      * @Route("/player/{slug}", name="player_show")
      * @param $slug
---
+++ src/Form/PlayerType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Player;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PlayerType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('age')
+            ->add('gamePlayed')
+            ->add('goalScored')
+            ->add('keyPass')
+            ->add('club')
+            ->add('country')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Player::class,
+        ]);
+    }
+}
