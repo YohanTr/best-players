bbd33df8ef594d33ef71577dac86f47d1e84bf92
Yohan Tahir <yohantahir@gmail.com>
2021-02-03 18:31:32 +0100

    top5 goal and keypass in home page

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\PlayerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,10 +11,16 @@ class HomeController extends AbstractController
 {
     /**
      * @Route("/", name="app_home")
+     * @param PlayerRepository $playerRepository
      * @return Response
      */
-    public function index(): Response
+    public function index(PlayerRepository $playerRepository): Response
     {
-        return $this->render('home.html.twig');
+
+        return $this->render('home.html.twig', [
+                'playerGoals' => $playerRepository->countGoal(10),
+                'playerKeyPass' => $playerRepository->countKeyPass(10)
+            ]
+        );
     }
 }
--- src/Repository/PlayerRepository.php
+++ src/Repository/PlayerRepository.php
@@ -19,22 +19,26 @@ class PlayerRepository extends ServiceEntityRepository
         parent::__construct($registry, Player::class);
     }
 
-    // /**
-    //  * @return Player[] Returns an array of Player objects
-    //  */
-    /*
-    public function findByExampleField($value)
+
+    public function countGoal($limit)
     {
         return $this->createQueryBuilder('p')
-            ->andWhere('p.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('p.id', 'ASC')
-            ->setMaxResults(10)
+            ->orderBy('p.goalScored', 'DESC')
+            ->setMaxResults($limit)
             ->getQuery()
             ->getResult()
         ;
     }
-    */
+
+    public function countKeyPass($limit)
+    {
+        return $this->createQueryBuilder('p')
+            ->orderBy('p.keyPass', 'DESC')
+            ->setMaxResults($limit)
+            ->getQuery()
+            ->getResult()
+            ;
+    }
 
     /*
     public function findOneBySomeField($value): ?Player
