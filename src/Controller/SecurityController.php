43206d97fb0152f3daa4af0547caa6a2ca92b280
Yohan Tahir <yohantahir@gmail.com>
2021-02-04 21:43:21 +0100

    fix regsiter

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
+
 class SecurityController extends AbstractController
 {
     /**
@@ -31,6 +32,7 @@ class SecurityController extends AbstractController
      */
     public function logout()
     {
+
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
