cac0b5eb1d2dd69ca1890c0351ccdd4ee9863fd1
Yohan Tahir <yohantahir@gmail.com>
2021-02-05 01:42:06 +0100

    fix mailFrom

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -50,12 +50,12 @@ class RegistrationController extends AbstractController
             // generate a signed url and email it to the user
             $this->emailVerifier->sendEmailConfirmation('app_verify_email', $user,
                 (new TemplatedEmail())
-                    ->from(new Address('mailer_from', 'PlayerStats'))
+                    ->from(new Address('playerstats@me.com', 'PlayerStats'))
                     ->to($user->getEmail())
                     ->subject('Please Confirm your Email')
                     ->htmlTemplate('registration/confirmation_email.html.twig')
             );
-            // do anything else you need here, like send an email
+            $this->addFlash('success', 'Check your mail to confirm your account');
 
             return $guardHandler->authenticateUserAndHandleSuccess(
                 $user,
